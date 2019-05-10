ecdaefe0bb0f19bacde1af91ea34ed6a2624325c
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2019-05-10 09:34:56 -0500

    Add files via upload

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,12 @@
-$('.menu').addClass('animated fadeIn');
+$('#menu').addClass('animated fadeIn delay-3s');
+$('#title-sub').addClass('animated fadeIn delay-1s');
+$('#title-name').addClass('animated fadeIn delay-2s');
 $('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
-  nav:true,
+  nav:false,
+  autoplay:true,
+  autoplayTimeout: 8000,
   responsive:{
       0:{
           items:1
@@ -15,17 +19,29 @@ $('.owl-carousel').owlCarousel({
       }
   }
 });
-$('.project-button').click(function() {
-  $('.menu').addClass('animated fadeOut');
-  //$('.menu').fadeOut("fast");
-  $('.projects').fadeIn(1000);
-});
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+    let a = [2, 1, 3, 5, 3, 2];
+
+
+
 
 $('.project-button').click(function() {
-  $('.menu').addClass('animated fadeOut');
-  //$('.menu').fadeOut("fast");
+  $('#menu').removeClass('fadeIn delay-3s').addClass('animated fadeOut');
   $('.projects').fadeIn(1000);
+  $('.owl-carousel').addClass('animated fadeIn delay-1s');
+  
+  //$('.menu').fadeOut("fast");
+  
 });
+
+$('.contact-button').click(function() {
+    $('#menu').removeClass('fadeIn delay-3s').addClass('animated fadeOut');
+    $('.contact').fadeIn(1000);
+});
+
+
 
 $('.trigger-contact').click(function() {
   $('.contact').toggleClass('close');
@@ -36,10 +52,4 @@ var parallaxInstance = new Parallax(scene);
 
 var scene2 = document.getElementById('medfnu');
 var parallaxInstance = new Parallax(scene2);
-
-
-
-
-
-
-
+});
